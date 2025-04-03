5de14a4030988bec7b765c4c9cbcdfee511a36ee
Christoph <367712+folkengine@users.noreply.github.com>
2025-04-03 08:53:15 -0700

    This branch sucks

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,6 +2,6 @@ import pytest
 from poetest.main import boop  # Replace with the actual function to test
 
 def test_your_function():
-    assert 1 == 1 # Replace with actual test logic and expected value
+    assert 1 == 2 # Replace with actual test logic and expected value
 
 # Add more test cases as needed
